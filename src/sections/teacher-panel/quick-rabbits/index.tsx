030f7d11986c4a1c2184bf74126d1ab7fe697378
Fabian Koder <fabian.koder@nextglobe.net>
2023-09-20 21:31:23 +0200

    Added missing socket disconnection on rabit close

--- src/sections/teacher-panel/quick-rabbits/index.tsx
+++ src/sections/teacher-panel/quick-rabbits/index.tsx
@@ -10,6 +10,7 @@ import { QuickRabbitsSideList } from "./quick-rabbits-side-list";
 import { RootState, useSelector } from "src/redux/store";
 import { useDispatch } from "react-redux";
 import { removeRabit } from "src/redux/slices/rabits";
+import {io} from "socket.io-client";
 
 export const QuickRabbits = (): React.ReactElement => {
   const theme = useTheme();
@@ -17,6 +18,8 @@ export const QuickRabbits = (): React.ReactElement => {
   const { query, push } = useRouter();
   const rabits = useSelector((state: RootState) => state.rabits.items);
   const onClose = (userId: string): void => {
+    // This is working, but not very pretty. We should unify all socket connections in a context, so we can access the socket here easily
+    io(process.env.NEXT_PUBLIC_CODE_STREAM_API ?? "", { path: "/" }).open().emit("leaveRoom", userId);
     dispatch(removeRabit({ id: userId }))
     if (userId === query?.id) {
       push(MANAGER_PATH_DASHBOARD.school.controller(query?.id as string));
